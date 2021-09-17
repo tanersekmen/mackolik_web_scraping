c31b344ea2bab2ca3e5b65db4dbede2530d42c62
tanersekmen <56790802+tanersekmen@users.noreply.github.com>
2021-09-17 11:41:42 +0300

    running the pulling data process

---
+++ run.py
@@ -0,0 +1,17 @@
+from data_scraping import scraping
+
+
+driver_path = 'C:/Users/tnr_s/Desktop/chromedriver.exe'  
+option = "--window-size=1920,1200"
+mackolik = scraping(driver_path = driver_path, option = option)
+mackolik.teams()
+mackolik.full_time_home()
+mackolik.full_time_zero()
+mackolik.full_time_away()
+mackolik.under()
+mackolik.over()
+mackolik.zero_or_one()
+mackolik.zero_or_two()
+mackolik.one_or_two()
+mackolik.both_teams_to_score()
+mackolik.both_teams_no_score()
